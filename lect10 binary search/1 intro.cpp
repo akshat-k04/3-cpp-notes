858a7a93ee2ec5c7d960b08510e4056e8a0b66a5
Akshat <a_khandelwal@ch.iitr.ac.in>
2022-12-14 18:59:27 +0530

    adding the binary search folder

---
+++ lect10 binary search/1 intro.cpp
@@ -0,0 +1,13 @@
+ // binary search is applied on monotonus function only 
+// let us understand this with example of searching a element from sorted array
+
+  #include <bits/stdc++.h>
+ using namespace std ;
+ int main(){
+    int n ;
+    cin>> n ;
+    vector<int> a(n) ;
+    for(int e =0 ; e<n ; e++){
+        
+    }
+ }
